6665b69e49438fb8c9e9301543326e737397a69a
paul <code@getbraintree.com>
2010-03-30 15:55:12 -0500

    Added more docs.  Made a method private that was not used outside the class.

--- braintree/validation_error_collection.py
+++ braintree/validation_error_collection.py
@@ -7,44 +7,58 @@ class ValidationErrorCollection(object):
         self.data = data
 
     def all(self):
+        """
+        Return all :class:`ValidationErrors <braintree.validatition_error.ValidationError>`, including nested errors.
+        """
+
         result = []
         result.extend(self.errors)
-        for nested_error in self.nested_errors.values():
+        for nested_error in self.__nested_errors.values():
             result.extend(nested_error.all())
         return result
 
     def for_object(self, nested_key):
+        """
+        Returns a :class:`ValidationErrorCollection <braintree.validation_error_collection.ValidationErrorCollection` representing the errors at the nested level:::
+
+            error_result = Transaction.sale({"credit_card": {"number": "invalid"}})
+            print error_result.errors.for_object("transaction").for_object("credit_card").on("number")[0].code
+        """
+
         return self.__get_nested_errrors(nested_key)
 
     def on(self, attribute):
+        """
+        Returns the list of errors for a given attribute::
+
+            error_result = Transaction.sale({"credit_card": {"number": "invalid"}})
+            print [ error.code for error in error_result.errors.for_object("transaction").for_object("credit_card").on("number") ]
+        """
         return [error for error in self.errors if error.attribute == attribute]
 
     @property
     def deep_size(self):
+        """Returns the number of errors on this object and any nested objects."""
+
         size = len(self.errors)
-        for error in self.nested_errors.values():
+        for error in self.__nested_errors.values():
             size += error.deep_size
         return size
 
     @property
     def errors(self):
+        """Returns a list of :class:`ValidationError <braintree.validatition_error.ValidationError>` objects."""
+
         return [ValidationError(error) for error in self.data["errors"]]
 
     @property
-    def nested_errors(self):
-        nested_errors = {}
-        for key in self.data.keys():
-            if key == "errors": continue
-            nested_errors[key] = ValidationErrorCollection(self.data[key])
-        return nested_errors
-
-    @property
     def size(self):
+        """Returns the number of errors on this object, without counting nested errors."""
         return len(self.errors)
 
     def __get_nested_errrors(self, nested_key):
-        if nested_key in self.nested_errors:
-            return self.nested_errors[nested_key]
+        if nested_key in self.__nested_errors:
+            return self.__nested_errors[nested_key]
         else:
             return ValidationErrorCollection()
 
@@ -53,3 +67,12 @@ class ValidationErrorCollection(object):
 
     def __len__(self):
         return self.size
+
+    @property
+    def __nested_errors(self):
+        nested_errors = {}
+        for key in self.data.keys():
+            if key == "errors": continue
+            nested_errors[key] = ValidationErrorCollection(self.data[key])
+        return nested_errors
+
