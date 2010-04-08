d624131e2a03e92c7b990cb144724df6b2e298a2
dan and patrick <code@getbraintree.com>
2010-04-08 16:33:12 -0500

    split TextNode into TextNodeBuilder and TextNode for searching, same for MultipleValueNode

--- braintree/search.py
+++ braintree/search.py
@@ -1,59 +1,49 @@
 class Search:
-    class TextNode(object):
+    class TextNodeBuilder(object):
         def __init__(self, name):
             self.name = name
-            self.dict = {}
 
         def __eq__(self, value):
             return self.is_equal(value)
 
         def is_equal(self, value):
-            clone = self.clone()
-            clone.dict["is"] = value
-            return clone
+            return Search.TextNode(self.name, {"is": value})
 
         def __ne__(self, value):
             return self.is_not_equal(value)
 
         def is_not_equal(self, value):
-            clone = self.clone()
-            clone.dict["is_not"] = value
-            return clone
+            return Search.TextNode(self.name, {"is_not": value})
 
         def starts_with(self, value):
-            clone = self.clone()
-            clone.dict["starts_with"] = value
-            return clone
+            return Search.TextNode(self.name, {"starts_with": value})
 
         def ends_with(self, value):
-            clone = self.clone()
-            clone.dict["ends_with"] = value
-            return clone
+            return Search.TextNode(self.name, {"ends_with": value})
 
         def contains(self, value):
-            clone = self.clone()
-            clone.dict["contains"] = value
-            return clone
+            return Search.TextNode(self.name, {"contains": value})
+
+    class TextNode(object):
+        def __init__(self, name, dict):
+          self.name = name
+          self.dict = dict
 
         def to_param(self):
             return self.dict
 
-        def clone(self):
-            return Search.TextNode(self.name)
-
-    class MultipleValueNode(object):
+    class MultipleValueNodeBuilder(object):
         def __init__(self, name):
             self.name = name
-            self.items = []
 
         def in_list(self, list):
-            clone = self.clone()
-            clone.items = list
-            return clone
+            return Search.MultipleValueNode(self.name, list)
+
+    class MultipleValueNode(object):
+        def __init__(self, name, items):
+            self.name = name
+            self.items = items
 
         def to_param(self):
             return self.items
-
-        def clone(self):
-            return Search.MultipleValueNode(self.name)
 
--- braintree/subscription_search.py
+++ braintree/subscription_search.py
@@ -1,6 +1,6 @@
 from braintree.search import Search
 
 class SubscriptionSearch:
-    plan_id = Search.TextNode("plan_id")
-    days_past_due = Search.TextNode("days_past_due")
-    status = Search.MultipleValueNode("status")
+    plan_id = Search.TextNodeBuilder("plan_id")
+    days_past_due = Search.TextNodeBuilder("days_past_due")
+    status = Search.MultipleValueNodeBuilder("status")
