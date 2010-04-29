64f414389b3129658c18660f935a79469cea8c95
patrick and paul <code@getbraintree.com>
2010-04-29 15:26:26 -0500

    Use setattr instead of __dict__

--- braintree/attribute_getter.py
+++ braintree/attribute_getter.py
@@ -1,4 +1,4 @@
 class AttributeGetter(object):
     def __init__(self, attributes={}):
         for key, val in attributes.iteritems():
-            self.__dict__[key] = val
+            setattr(self, key, val)
