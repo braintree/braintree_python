243ba3554d018c2724f30b323ec8e26b5389d59d
ali and lihsuan <code@getbraintree.com>
2011-09-27 16:39:19 -0500

    [#2626] - added processor_settings error codes for transactions

--- braintree/error_codes.py
+++ braintree/error_codes.py
@@ -181,6 +181,8 @@ class ErrorCodes(object):
         PaymentMethodTokenIsInvalid = "91518"
         ProcessorAuthorizationCodeCannotBeSet = "91519"
         ProcessorAuthorizationCodeIsInvalid = "81520"
+        ProcessorDoesNotSupportCredits = "91546"
+        ProcessorDoesNotSupportVoiceAuthorizations = "91545"
         PurchaseOrderNumberIsTooLong = "91537"
         RefundAmountIsTooLarge = "91521"
         SettlementAmountIsTooLarge = "91522"
