4e3478301de26ca30d73f72ba4a7def7dd540221
ali and lihsuan <code@getbraintree.com>
2012-02-28 10:44:31 -0600

    [#3097] - Add the ability to find duplicated credit cards by token.

--- braintree/credit_card.py
+++ braintree/credit_card.py
@@ -132,6 +132,11 @@ class CreditCard(Resource):
         return Configuration.gateway().credit_card.delete(credit_card_token)
 
     @staticmethod
+    def duplicates(credit_card_token):
+        """ Return a collection of duplicated credit cards. """
+        return Configuration.gateway().credit_card.duplicates(credit_card_token)
+
+    @staticmethod
     def expired():
         """ Return a collection of expired credit cards. """
         return Configuration.gateway().credit_card.expired()
