db7878f2961f68d93971faf3317cd6766324b17d
cory and dan <code@getbraintree.com>
2012-04-09 11:46:08 -0500

    yellow - extract the current http request implementation into a separate strategy class to prepare for implementing multiple strategies

--- braintree/util/http.py
+++ braintree/util/http.py
@@ -1,7 +1,7 @@
-import httplib
 import base64
-from braintree.configuration import Configuration
+from braintree import version
 from braintree.util.xml_util import XmlUtil
+from braintree.configuration import Configuration
 from braintree.exceptions.authentication_error import AuthenticationError
 from braintree.exceptions.authorization_error import AuthorizationError
 from braintree.exceptions.down_for_maintenance_error import DownForMaintenanceError
@@ -9,7 +9,7 @@ from braintree.exceptions.not_found_error import NotFoundError
 from braintree.exceptions.server_error import ServerError
 from braintree.exceptions.unexpected_error import UnexpectedError
 from braintree.exceptions.upgrade_required_error import UpgradeRequiredError
-from braintree import version
+from braintree.util.http_strategy.pycurl_strategy import PycurlStrategy
 
 class Http(object):
     @staticmethod
@@ -50,31 +50,18 @@ class Http(object):
         return self.__http_do("PUT", path, params)
 
     def __http_do(self, http_verb, path, params=None):
-        if self.environment.is_ssl:
-            self.__verify_ssl()
-            conn = httplib.HTTPSConnection(self.environment.server, self.environment.port)
-        else:
-            conn = httplib.HTTPConnection(self.environment.server, self.environment.port)
-
-        conn.request(
-            http_verb,
-            self.config.base_merchant_path() + path,
-            XmlUtil.xml_from_dict(params) if params else '',
-            self.__headers()
-        )
-        response = conn.getresponse()
-        status = response.status
+        pycurl_strategy = PycurlStrategy(self.config, self.environment)
+        request_body = XmlUtil.xml_from_dict(params) if params else ''
+        full_path = self.config.base_merchant_path() + path
+        status, response_body = pycurl_strategy.http_do(http_verb, full_path, self.__headers(), request_body)
 
         if Http.is_error_status(status):
-            conn.close()
             Http.raise_exception_from_status(status)
         else:
-            data = response.read()
-            conn.close()
-            if len(data.strip()) == 0:
+            if len(response_body.strip()) == 0:
                 return {}
             else:
-                return XmlUtil.dict_from_xml(data)
+                return XmlUtil.dict_from_xml(response_body)
 
     def __authorization_header(self):
         return "Basic " + base64.encodestring(self.config.public_key + ":" + self.config.private_key).strip()
@@ -88,30 +75,3 @@ class Http(object):
             "X-ApiVersion": Configuration.api_version()
         }
 
-    def __verify_ssl(self):
-        if Configuration.use_unsafe_ssl: return
-
-        try:
-            import pycurl
-        except ImportError, e:
-            print "Cannot load PycURL.  Please refer to Braintree documentation."
-            print """
-If you are in an environment where you absolutely cannot load PycURL
-(such as Google App Engine), you can turn off SSL Verification by setting:
-
-    Configuration.use_unsafe_ssl = True
-
-This is highly discouraged, however, since it leaves you susceptible to
-man-in-the-middle attacks."""
-            raise e
-
-        curl = pycurl.Curl()
-        # see http://curl.haxx.se/libcurl/c/curl_easy_setopt.html for info on these options
-        curl.setopt(pycurl.CAINFO, self.environment.ssl_certificate)
-        curl.setopt(pycurl.ENCODING, 'gzip')
-        curl.setopt(pycurl.SSL_VERIFYPEER, 1)
-        curl.setopt(pycurl.SSL_VERIFYHOST, 2)
-        curl.setopt(pycurl.NOBODY, 1)
-        curl.setopt(pycurl.NOSIGNAL, 1)
-        curl.setopt(pycurl.URL, self.environment.protocol + self.environment.server_and_port)
-        curl.perform()
---
+++ braintree/util/http_strategy/pycurl_strategy.py
@@ -0,0 +1,49 @@
+import httplib
+from braintree.configuration import Configuration
+
+class PycurlStrategy(object):
+    def __init__(self, config, environment):
+        self.config = config
+        self.environment = environment
+
+    def http_do(self, http_verb, path, headers, request_body):
+        if self.environment.is_ssl:
+            self.__verify_ssl()
+            conn = httplib.HTTPSConnection(self.environment.server, self.environment.port)
+        else:
+            conn = httplib.HTTPConnection(self.environment.server, self.environment.port)
+
+        conn.request(http_verb, path, request_body, headers)
+        response = conn.getresponse()
+        status = response.status
+        response_body = response.read()
+        conn.close()
+        return [status, response_body]
+
+    def __verify_ssl(self):
+        if Configuration.use_unsafe_ssl: return
+
+        try:
+            import pycurl
+        except ImportError, e:
+            print "Cannot load PycURL.  Please refer to Braintree documentation."
+            print """
+If you are in an environment where you absolutely cannot load PycURL
+(such as Google App Engine), you can turn off SSL Verification by setting:
+
+    Configuration.use_unsafe_ssl = True
+
+This is highly discouraged, however, since it leaves you susceptible to
+man-in-the-middle attacks."""
+            raise e
+
+        curl = pycurl.Curl()
+        # see http://curl.haxx.se/libcurl/c/curl_easy_setopt.html for info on these options
+        curl.setopt(pycurl.CAINFO, self.environment.ssl_certificate)
+        curl.setopt(pycurl.ENCODING, 'gzip')
+        curl.setopt(pycurl.SSL_VERIFYPEER, 1)
+        curl.setopt(pycurl.SSL_VERIFYHOST, 2)
+        curl.setopt(pycurl.NOBODY, 1)
+        curl.setopt(pycurl.NOSIGNAL, 1)
+        curl.setopt(pycurl.URL, self.environment.protocol + self.environment.server_and_port)
+        curl.perform()
