7f89f0a48bd626a0d3bca1fe6a515cdf9ff6b7d4
dan and patrick <code@getbraintree.com>
2012-04-23 09:55:04 -0500

    bugfix - include braintree.util.http_strategy package in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author="Braintree",
     author_email="support@braintreepayments.com",
     url="https://www.braintreepayments.com/docs/python",
-    packages=["braintree", "braintree.exceptions", "braintree.util"],
+    packages=["braintree", "braintree.exceptions", "braintree.util", "braintree.util.http_strategy"],
     package_data={"braintree": ["ssl/*"]},
     install_requires=["requests>=0.11.1,<1.0"],
     zip_safe=False
