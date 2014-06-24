0b64f92892d751e181e6c2024f9d430e1a427b80
adam <code@getbraintree.com>
2014-06-24 20:58:53 +0000

    Update ci.sh for testing on Python 3 in CI.

--- ci.sh
+++ ci.sh
@@ -4,4 +4,8 @@ if [[ "$1" == "http" ]]; then
   python_tests="test:http"
 fi
 
-env PATH=/usr/local/python/2.7.1/bin:$PATH rake $python_tests --trace
+if [[ "$1" == "python3" ]]; then
+  /usr/local/lib/python3.3/bin/nosetests-3.3
+else
+  env PATH=/usr/local/python/2.7.1/bin:$PATH rake $python_tests --trace
+fi
