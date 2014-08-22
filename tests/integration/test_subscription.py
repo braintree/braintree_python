600db1d93f95c8db4aeac081d92dda17932d89f9
gary and linda <code@getbraintree.com>
2014-08-22 18:56:21 +0000

    interruptions tNfMJpW5 - Validate that Subscriptions exposes timestamps

--- tests/integration/test_subscription.py
+++ tests/integration/test_subscription.py
@@ -43,6 +43,9 @@ class TestSubscription(unittest.TestCase):
         self.assertEquals(date, type(subscription.billing_period_end_date))
         self.assertEquals(date, type(subscription.paid_through_date))
 
+        self.assertEquals(unicode, type(subscription.created_at))
+        self.assertEquals(unicode, type(subscription.updated_at))
+        
         self.assertEquals(1, subscription.current_billing_cycle)
         self.assertEquals(0, subscription.failure_count)
         self.assertEquals(self.credit_card.token, subscription.payment_method_token)
