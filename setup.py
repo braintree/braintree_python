fe4e15ec1d5b92a5b4d86fb31476ddb7c160cccc
billr and dewzy <code@getbraintree.com>
2015-06-02 15:57:38 +0000

    Bumped version and updated changelog

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
     name="braintree",
-    version="3.14.0",
+    version="3.15.0",
     description="Braintree Python Library",
     author="Braintree",
     author_email="support@braintreepayments.com",
