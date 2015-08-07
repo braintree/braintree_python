2f6c5d0397f96f223b4c5562fff82b79917fab21
Ionuț Arțăriși <ionut@artarisi.eu>
2015-08-07 20:06:10 +0100

    extract test fixture to class attribute
    It's useful for more than one test case.

--- tests/unit/test_resource_collection.py
+++ tests/unit/test_resource_collection.py
@@ -1,6 +1,13 @@
 from tests.test_helper import *
 
 class TestResourceCollection(unittest.TestCase):
+    collection_data = {
+        "search_results": {
+            "page_size": 2,
+            "ids": ["0", "1", "2", "3", "4"]
+        }
+    }
+
     class TestResource:
         items = ["a", "b", "c", "d", "e"]
 
@@ -9,13 +16,7 @@ class TestResourceCollection(unittest.TestCase):
             return [TestResourceCollection.TestResource.items[int(id)] for id in ids]
 
     def test_iterating_over_contents(self):
-        collection_data = {
-            "search_results": {
-                "page_size": 2,
-                "ids": ["0", "1", "2", "3", "4"]
-            }
-        }
-        collection = ResourceCollection("some_query", collection_data, TestResourceCollection.TestResource.fetch)
+        collection = ResourceCollection("some_query", self.collection_data, TestResourceCollection.TestResource.fetch)
         new_items = []
         index = 0
         for item in collection.items:
