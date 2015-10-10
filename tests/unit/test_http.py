97efb256bc4b9c88eadc7d3951daa2c5896ecd37
adam <code@getbraintree.com>
2015-10-10 18:57:39 +0000

    cleanup - Preserve backtrace when not wrapping http exceptions

--- tests/unit/test_http.py
+++ tests/unit/test_http.py
@@ -1,4 +1,7 @@
+import traceback
+
 from tests.test_helper import *
+from braintree.attribute_getter import AttributeGetter
 
 class TestHttp(unittest.TestCase):
     def test_raise_exception_from_status_for_upgrade_required(self):
@@ -7,3 +10,24 @@ class TestHttp(unittest.TestCase):
             self.assertTrue(False)
         except UpgradeRequiredError:
             pass
+
+    def test_backtrace_preserved_when_not_wrapping_exceptions(self):
+        class Error(Exception):
+            pass
+        def raise_error(*_):
+            raise Error
+        http_strategy = AttributeGetter({"http_do": raise_error})
+        config = AttributeGetter({
+                "base_url": (lambda: ""),
+                "has_access_token": (lambda: False),
+                "has_client_credentials": (lambda: False),
+                "http_strategy": (lambda: http_strategy),
+                "public_key": "",
+                "private_key": "",
+                "wrap_http_exceptions": False})
+
+        try:
+            Http(config, "a")._Http__http_do("a", "b")
+        except Error as e:
+            _, _, tb = sys.exc_info()
+            self.assertEqual('raise_error', traceback.extract_tb(tb)[-1][2])
