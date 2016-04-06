7c5f5ab74f5868cee8bdf88266d316985d46aeb8
jziaja and zsrog <code@getbraintree.com>
2016-04-06 18:36:25 +0000

    partners yHvq3VZS - Add support to pass currencies to merchant create

--- braintree/error_codes.py
+++ braintree/error_codes.py
@@ -180,10 +180,12 @@ class ErrorCodes(object):
         CountryCodeNumericIsNotAccepted = "93608"
         CountryNameIsInvalid = "93611"
         CountryNameIsNotAccepted = "93610"
+        CurrenciesAreInvalid = "93614"
         EmailFormatIsInvalid = "93602"
         EmailIsRequired = "83601"
         InconsistentCountry = "93612"
         PaymentMethodsAreInvalid = "93613"
+        PaymentMethodsAreNotAllowed = "93615"
 
     class MerchantAccount(object):
         IdFormatIsInvalid = "82603"
