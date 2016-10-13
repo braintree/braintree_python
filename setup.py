ef6c1bdea1693867c6548ef0087650c954c16745
Adam Chainz <adam@adamj.eu>
2016-10-13 15:58:27 +0100

    setup.py improvements
    I visited the PyPI page as I was looking at dependencies to upgrade and found it a little bare, so I've made these changes:
    
    - Include README so visitors to PyPI page can find links easily. This requires converting the README to reStructuredText since that's all PyPI supports.
    - Add a link from the README direct to the Github repository, since the `url` field is used to link to the braintree docs.
    - Use `setuptools` rather than `distutils`, because the future is now.

--- setup.py
+++ setup.py
@@ -1,16 +1,28 @@
-from distutils.core import setup
+from setuptools import setup
+
+with open('README.rst', 'r') as fp:
+    long_description = fp.read()
+
 setup(
     name="braintree",
     version="3.29.2",
     description="Braintree Python Library",
+    long_description=long_description,
     author="Braintree",
     author_email="support@braintreepayments.com",
     url="https://developers.braintreepayments.com/python/sdk/server/overview",
-    packages=["braintree", "braintree.exceptions", "braintree.exceptions.http", "braintree.merchant_account", "braintree.util", "braintree.test"],
+    packages=[
+        "braintree",
+        "braintree.exceptions",
+        "braintree.exceptions.http",
+        "braintree.merchant_account",
+        "braintree.util",
+        "braintree.test"
+    ],
     package_data={"braintree": ["ssl/*"]},
     install_requires=["requests>=0.11.1,<3.0"],
     zip_safe=False,
-    classifiers = [
+    classifiers=[
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.3",
