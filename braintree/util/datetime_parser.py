a876fd476a2de9189eb6331dd83c47390da69e75
evanhahn and sdcoffey <code@getbraintree.com>
2017-04-20 20:34:25 +0000

    BTDXCLTS-515 - Support fractional seconds in date parser

--- braintree/util/datetime_parser.py
+++ braintree/util/datetime_parser.py
@@ -3,21 +3,31 @@ from datetime import datetime
 from datetime import timedelta
 
 
+_OFFSET_REGEX = re.compile(r'(\+|\-)(\d\d):(\d\d)$')
+_SYMBOLS_REGEX = re.compile(r'[-:Z]')
+
+
 def parse_datetime(timestamp):
-    if timestamp[-1] == 'Z':
-        timestamp = timestamp.replace('-', '').replace(':', '')
-        return datetime.strptime(timestamp, '%Y%m%dT%H%M%SZ')
+    offset_matches = _OFFSET_REGEX.findall(timestamp)
+
+    if len(offset_matches) == 0:
+        timestamp = _SYMBOLS_REGEX.sub('', timestamp)
+        without_seconds = datetime.strptime(timestamp[:13], '%Y%m%dT%H%M')
+        seconds = timedelta(seconds=float(timestamp[13:]))
+        return without_seconds + seconds
     else:
+        time_without_offset = parse_datetime(timestamp[:-6])
+
         try:
-            delta_matches = re.findall(r'(\+|\-)(\d\d):(\d\d)$', timestamp)[0]
-            delta_is_negative = delta_matches[0] == '-'
-            delta_hours = int(delta_matches[1])
-            delta_minutes = int(delta_matches[2])
+            offset_matches = offset_matches[0]
+            offset_is_negative = offset_matches[0] == '-'
+            offset_hours = int(offset_matches[1])
+            offset_minutes = int(offset_matches[2])
         except IndexError:
             pass
-        parsed = datetime.strptime(timestamp[:-6], '%Y-%m-%dT%H:%M:%S')
-        delta = timedelta(hours=delta_hours, minutes=delta_minutes)
-        if delta_is_negative:
-            return parsed + delta
+        offset = timedelta(hours=offset_hours, minutes=offset_minutes)
+
+        if offset_is_negative:
+            return time_without_offset + offset
         else:
-            return parsed - delta
+            return time_without_offset - offset
--- tests/unit/util/test_datetime_parser.py
+++ tests/unit/util/test_datetime_parser.py
@@ -1,31 +1,31 @@
 import unittest
-from braintree.util.datetime_parser import parse_datetime
+from braintree.util.datetime_parser import parse_datetime as parse
 from datetime import datetime
 
 
 class TestDateParser(unittest.TestCase):
     def test_parses_with_zulu_and_symbols(self):
-        result = parse_datetime('2017-04-19T18:51:21Z')
-        self.assertEqual(result, datetime(2017, 4, 19, 18, 51, 21))
+        self.assertEqual(parse('2017-04-19T18:51:21Z'), datetime(2017, 4, 19, 18, 51, 21))
+        self.assertEqual(parse('2017-04-19T18:51:21.45Z'), datetime(2017, 4, 19, 18, 51, 21, 450000))
 
     def test_parses_with_zulu_and_no_symbols(self):
-        result = parse_datetime('20170419T185121Z')
-        self.assertEqual(result, datetime(2017, 4, 19, 18, 51, 21))
+        self.assertEqual(parse('20170419T185121Z'), datetime(2017, 4, 19, 18, 51, 21))
+        self.assertEqual(parse('20170419T185121.123Z'), datetime(2017, 4, 19, 18, 51, 21, 123000))
 
     def test_parses_with_zero_offset(self):
-        result = parse_datetime('2017-04-19T18:51:21+00:00')
-        self.assertEqual(result, datetime(2017, 4, 19, 18, 51, 21))
+        self.assertEqual(parse('2017-04-19T18:51:21+00:00'), datetime(2017, 4, 19, 18, 51, 21))
+        self.assertEqual(parse('2017-04-19T18:51:21.420+00:00'), datetime(2017, 4, 19, 18, 51, 21, 420000))
 
     def test_parses_with_negative_offset(self):
-        result = parse_datetime('2017-04-19T18:51:21-01:30')
-        self.assertEqual(result, datetime(2017, 4, 19, 20, 21, 21))
+        self.assertEqual(parse('2017-04-19T18:51:21-01:30'), datetime(2017, 4, 19, 20, 21, 21))
+        self.assertEqual(parse('2017-04-19T18:51:21.987-01:30'), datetime(2017, 4, 19, 20, 21, 21, 987000))
 
     def test_parses_with_positive_offset(self):
-        result = parse_datetime('2017-04-19T18:51:21+07:00')
-        self.assertEqual(result, datetime(2017, 4, 19, 11, 51, 21))
+        self.assertEqual(parse('2017-04-19T18:51:21+07:00'), datetime(2017, 4, 19, 11, 51, 21))
+        self.assertEqual(parse('2017-04-19T18:51:21.765+07:00'), datetime(2017, 4, 19, 11, 51, 21, 765000))
 
     def test_raises_with_bad_input(self):
         with self.assertRaises(ValueError):
-            parse_datetime('20170420')
+            parse('20170420')
         with self.assertRaises(ValueError):
-            parse_datetime('20170420Z')
+            parse('20170420Z')
