eb51ea1faeaa793e196132f10966a1d0b8fb0223
lkorth and sdcoffey <code@getbraintree.com>
2017-05-01 22:08:19 +0000

    Bumped version and updated changelog

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = """
 
 setup(
     name="braintree",
-    version="3.37.0",
+    version="3.37.1",
     description="Braintree Python Library",
     long_description=long_description,
     author="Braintree",
