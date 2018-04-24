f3071d7e1fcb834f2c6f3d9ae6c3095161aa351e
Riley Rook <riley.rook@getbraintree.com>
2018-04-24 20:54:29 +0000

    BTTEXPAN-371 - Update python sdk with disbursement_type.

--- tests/unit/test_disbursement.py
+++ tests/unit/test_disbursement.py
@@ -2,27 +2,41 @@ from tests.test_helper import *
 from datetime import date
 
 class TestDisbursement(unittest.TestCase):
-    def test_constructor(self):
-        attributes = {
-            "merchant_account": {
-                "id": "sub_merchant_account",
-                "status": "active",
-                "master_merchant_account": {
-                    "id": "master_merchant_account",
-                    "status": "active"
-                },
+    attributes = {
+        "merchant_account": {
+            "id": "sub_merchant_account",
+            "status": "active",
+            "master_merchant_account": {
+                "id": "master_merchant_account",
+                "status": "active"
             },
-            "id": "123456",
-            "exception_message": "invalid_account_number",
-            "amount": "100.00",
-            "disbursement_date": date(2013, 4, 10),
-            "follow_up_action": "update",
-            "transaction_ids": ["asdf", "qwer"]
-        }
+        },
+        "id": "123456",
+        "exception_message": "invalid_account_number",
+        "amount": "100.00",
+        "disbursement_date": date(2013, 4, 10),
+        "follow_up_action": "update",
+        "transaction_ids": ["asdf", "qwer"],
+        "disbursement_type": "credit"
+    }
 
-        disbursement = Disbursement(None, attributes)
+    def test_constructor(self):
+        disbursement = Disbursement(None, TestDisbursement.attributes)
 
         self.assertEqual("123456", disbursement.id)
         self.assertEqual(Decimal("100.00"), disbursement.amount)
         self.assertEqual(["asdf", "qwer"], disbursement.transaction_ids)
         self.assertEqual("master_merchant_account", disbursement.merchant_account.master_merchant_account.id)
+
+    def test_credit(self):
+        disbursement = Disbursement(None, TestDisbursement.attributes)
+
+        self.assertTrue(disbursement.is_credit())
+
+    def test_debit(self):
+        thing = TestDisbursement.attributes
+        thing["disbursement_type"] = "debit"
+
+        disbursement = Disbursement(None, TestDisbursement.attributes)
+
+        self.assertTrue(disbursement.is_debit())
