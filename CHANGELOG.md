8e7e610af5273204196b7278cb26394e7baee134
Chrissie Deist <chrissie.deist@getbraintree.com>
2019-07-19 21:40:16 +0000

    Revert "Revert "BTFT-1088 Add additional 3DS response fields to 3DS info""
    This reverts commit 13366009bb8a5e80d55ccb7b25321fdd1c1d3beb.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@
 * Add `refundFromTransactionFeeAmount` field to local_payment_details
 * Add `refundFromTransactionFeeCurrencyIsoCode` field to local_payment_details
 * Add `ds_transaction_id` and `three_d_secure_version` to 3DS pass thru fields
+* Add `cavv`, `xid`, `ds_transaction_id`, `eci_flag`, and `three_d_secure_version`, to `three_d_secure_info`
 * Add `payer_info` field to payment_method_nonce details
 * Add more specific timeout errors: (#105 thanks @bhargavrpatel)
 * Add `braintree.exceptions.http.timeout_error.ConnectTimeoutError` (child class of TimeoutError)
--- tests/unit/test_three_d_secure_info.py
+++ tests/unit/test_three_d_secure_info.py
@@ -8,9 +8,19 @@ class TestThreeDSecureInfo(unittest.TestCase):
             "status": "authenticate_successful",
             "liability_shifted": True,
             "liability_shift_possible": True,
+            "cavv": "some_cavv",
+            "xid": "some_xid",
+            "ds_transaction_id": "some_ds_txn_id",
+            "eci_flag": "07",
+            "three_d_secure_version": "1.0.2",
         })
 
         self.assertEqual("Y", three_d_secure_info.enrolled)
         self.assertEqual("authenticate_successful", three_d_secure_info.status)
         self.assertEqual(True, three_d_secure_info.liability_shifted)
         self.assertEqual(True, three_d_secure_info.liability_shift_possible)
+        self.assertEqual("some_cavv", three_d_secure_info.cavv)
+        self.assertEqual("some_xid", three_d_secure_info.xid)
+        self.assertEqual("some_ds_txn_id", three_d_secure_info.ds_transaction_id)
+        self.assertEqual("07", three_d_secure_info.eci_flag)
+        self.assertEqual("1.0.2", three_d_secure_info.three_d_secure_version)
