520951fdf0addd02627d9fc9b109d5b9e5bfc6fb
wwerges & tmartinak <code@getbraintree.com>
2019-10-02 20:13:44 +0000

    Bumped version and updated changelog

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = """
 
 setup(
     name="braintree",
-    version="3.56.0",
+    version="3.57.1",
     description="Braintree Python Library",
     long_description=long_description,
     author="Braintree",
