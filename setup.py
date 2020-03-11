63078819031cf05afd8557c2819d36444742ca39
Holly Stotelmyer <holly.stotelmyer@getbraintree.com>
2020-03-11 22:03:58 +0000

    Fixes for release script
    Signed-off-by: David Merino <david.merino@getbraintree.com>

--- setup.py
+++ setup.py
@@ -25,9 +25,9 @@ setup(
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.5"
-        "Programming Language :: Python :: 3.6"
-        "Programming Language :: Python :: 3.7"
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8"
     ]
 )
