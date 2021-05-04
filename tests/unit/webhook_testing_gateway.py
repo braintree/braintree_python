b7f84ceb83b7afcae748a94d9c5cc920f9151d2b
Marek Zieliński <marzielinski@paypal.com>
2021-05-04 19:59:48 +0000

    Fix unit test
    Signed-off-by: Matias Juarez <matjuarez@paypal.com>

--- tests/unit/webhook_testing_gateway.py
+++ tests/unit/webhook_testing_gateway.py
@@ -4,10 +4,9 @@ import xml.etree.ElementTree as ET
 
 class TestWebhookTestingGateway(unittest.TestCase):
     def test_granted_payment_method_revoked(self):
-        webhook_testing_gateway = WebhookTestingGateway(TestWebhookTestingGateway.__create_gateway())
+        webhook_testing_gateway = WebhookTestingGateway(BraintreeGateway(Configuration.instantiate()))
 
-        sample_notification = webhook_testing_gateway.sample_notification(
-                WebhookNotification.Kind.GrantedPaymentMethodRevoked, '1234')
+        sample_notification = webhook_testing_gateway.sample_notification(WebhookNotification.Kind.GrantedPaymentMethodRevoked, '1234')
 
         payload = base64.b64decode(sample_notification['bt_payload']).decode('UTF-8')
         tree = ET.fromstring(payload)
@@ -16,13 +15,3 @@ class TestWebhookTestingGateway(unittest.TestCase):
 
         self.assertEqual('venmo-account', subject.find('venmo-account').tag)
         self.assertEqual('1234', subject.find('venmo-account').find('token').text)
-
-    @staticmethod
-    def __create_gateway():
-        config = Configuration(
-            environment=Environment.Development,
-            merchant_id="integration_merchant_id",
-            public_key="integration_public_key",
-            private_key="integration_private_key"
-        )
-        return BraintreeGateway(config)
