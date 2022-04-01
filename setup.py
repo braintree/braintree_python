0df13231de4a8b0b0a62e0c63aacda66c5c34dd3
joe and tim <code@getbraintree.com>
2022-04-01 15:23:23 +0000

    Bumped version and updated changelog

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = """
 
 setup(
     name="braintree",
-    version="4.14.0",
+    version="4.15.0",
     description="Braintree Python Library",
     long_description=long_description,
     author="Braintree",
